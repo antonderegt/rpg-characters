542ebfb270ebea31b4525e6c527e97e933b58b46
Anton de Regt <antonderegt@pm.me>
2021-05-10 18:23:33 +0200

    Add weapon class

---
+++ RPGCharacters/Weapon.cs
@@ -0,0 +1,18 @@
+﻿namespace RPGCharacters
+{
+    public enum WeaponType
+    {
+        WEAPON_AXE,
+        WEAPON_BOW,
+        WEAPON_DAGGER,
+        WEAPON_HAMMER,
+        WEAPON_STAFF,
+        WEAPON_SWORD,
+        WEAPON_WAND
+    }
+    class Weapon : Item
+    {
+        public WeaponType WeaponType { get; set; }
+        public WeaponAttributes WeaponAttributes { get; set; }
+    }
+}
---
+++ RPGCharacters/WeaponAttributes.cs
@@ -0,0 +1,8 @@
+﻿namespace RPGCharacters
+{
+    public class WeaponAttributes
+    {
+        public int Damage { get; set; }
+        public int AttackSpeed { get; set; }
+    }
+}
