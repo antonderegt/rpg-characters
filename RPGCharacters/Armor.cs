689aa17dff3fffd8bdfbbcbb57c87ff746b3304c
Anton de Regt <antonderegt@pm.me>
2021-05-10 18:23:14 +0200

    Add armor class

---
+++ RPGCharacters/Armor.cs
@@ -0,0 +1,14 @@
+﻿namespace RPGCharacters
+{
+    public enum ArmorType { 
+        ARMOR_CLOTH,
+        ARMOR_LEATHER,
+        ARMOR_MAIL,
+        ARMOR_PLATE
+    }
+    class Armor : Item
+    {
+        public ArmorType ArmorType { get; set; }
+        public PrimaryAttributes Attributes { get; set; }
+    }
+}
