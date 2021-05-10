c9f0156a927c1cdfa0a2f1f1cda2c80d6d43c1f5
Anton de Regt <antonderegt@pm.me>
2021-05-10 15:16:12 +0200

    Add project files.

---
+++ RPGCharacters/Hero.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacters
+{
+    abstract class Hero
+    {
+        private string Name { get; set; }
+        protected int Level { get; set; }
+
+        // Base primary attributes
+        protected int BaseStrength { get; set; }
+        protected int BaseDexterity { get; set; }
+        protected int BaseIntelligence { get; set; }
+        protected int BaseVitality { get; set; }
+
+
+        // Total primary attributes
+        private int TotalStrength { get; set; }
+        private int TotalDexterity { get; set; }
+        private int TotalIntelligence { get; set; }
+        private int TotalVitality { get; set; }
+
+        // Secondary attributes
+        private int Health { get; set; }
+        private int ArmorRating { get; set; }
+        private int ElementalResistance { get; set; }
+
+
+        public Hero(string name, int strength, int dexterity, int intelligence, int vitality)
+        {
+            Name = name;
+            Level = 1;
+            BaseStrength = strength;
+            BaseDexterity = dexterity;
+            BaseIntelligence = intelligence;
+            BaseVitality = vitality;
+            Health = BaseVitality * 10;
+            ArmorRating = BaseStrength + BaseDexterity;
+            ElementalResistance = BaseIntelligence;
+        }
+
+        public abstract void LevelUp();
+    }
+}
---
+++ RPGCharacters/Mage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacters
+{
+    class Mage : Hero
+    {
+        public Mage(string name) : base(name, 5, 1, 1, 1)
+        {
+            Console.WriteLine("Created mage");
+        }
+
+        public override void LevelUp()
+        {
+            Level++;
+            BaseVitality += 3;
+            BaseStrength += 1;
+            BaseDexterity += 1;
+            BaseIntelligence += 5;
+
+            // Compute total attributes
+        }
+    }
+}
