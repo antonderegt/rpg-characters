7bb0ad2dfdeba554ab1ebe0eadeb82c5e3c26b7f
Anton de Regt <antonderegt@pm.me>
2021-05-10 20:50:24 +0200

    Add custom Exception classes

---
+++ RPGCharacters/InvalidArmorException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacters
+{
+    class InvalidArmorException : Exception
+    {
+        public InvalidArmorException()
+        {
+        }
+
+        public InvalidArmorException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidArmorException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ RPGCharacters/InvalidWeaponException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacters
+{
+    class InvalidWeaponException : Exception
+    {
+        public InvalidWeaponException()
+        {
+        }
+
+        public InvalidWeaponException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidWeaponException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
