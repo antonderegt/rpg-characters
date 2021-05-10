27f0d8db0b25066059eb21fe9bc9bf2a2e748e3a
Anton de Regt <antonderegt@pm.me>
2021-05-10 18:14:35 +0200

    Change property names

---
+++ RPGCharacters/Item.cs
@@ -0,0 +1,17 @@
+﻿namespace RPGCharacters
+{
+
+    public enum Slot
+    {
+        SLOT_HEADER,
+        SLOT_BODY,
+        SLOT_LEGS,
+        SLOT_WEAPON
+    }
+    class Item
+    {
+        public string ItemName { get; set; }
+        public int ItemLevel { get; set; }
+        public Slot ItemSlot { get; set; }
+    }
+}
