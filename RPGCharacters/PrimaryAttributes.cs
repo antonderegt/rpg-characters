474e2b617643b720dd4d891c98f099e3c4600358
Anton de Regt <antonderegt@pm.me>
2021-05-11 13:09:54 +0200

    Add operator+ overload

--- RPGCharacters/PrimaryAttributes.cs
+++ RPGCharacters/PrimaryAttributes.cs
@@ -6,5 +6,16 @@
         public int Dexterity { get; set; }
         public int Intelligence { get; set; }
         public int Vitality { get; set; }
+
+        public static PrimaryAttributes operator+ (PrimaryAttributes a, PrimaryAttributes b)
+        {
+            PrimaryAttributes result = new PrimaryAttributes();
+            result.Strength = a.Strength + b.Strength;
+            result.Dexterity = a.Dexterity + b.Dexterity;
+            result.Intelligence = a.Intelligence + b.Intelligence;
+            result.Vitality = a.Vitality + b.Vitality;
+
+            return result;
+        }
     }
 }
