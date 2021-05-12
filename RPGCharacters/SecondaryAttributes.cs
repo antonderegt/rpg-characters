1bb3a177544fb10d9a4ecef535f111b9a1dabd21
Anton de Regt <antonderegt@pm.me>
2021-05-12 14:26:14 +0200

    Implement IEquatable

--- RPGCharacters/SecondaryAttributes.cs
+++ RPGCharacters/SecondaryAttributes.cs
@@ -1,9 +1,23 @@
-﻿namespace RPGCharacters
+﻿using System;
+
+namespace RPGCharacters
 {
-    public class SecondaryAttributes
+    public class SecondaryAttributes : IEquatable<SecondaryAttributes>
     {
         public int Health { get; set; }
         public int ArmorRating { get; set; }
         public int ElementalResistence { get; set; }
+
+        public bool Equals(SecondaryAttributes other)
+        {
+            if (Health != other.Health)
+                return false;
+            if (ArmorRating != other.ArmorRating)
+                return false;
+            if (ElementalResistence != other.ElementalResistence)
+                return false;
+
+            return true;
+        }
     }
 }
