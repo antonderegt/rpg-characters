c0afd6b8880ba0645795ffcd36dc3041caa65ceb
Anton de Regt <antonderegt@pm.me>
2021-05-14 13:54:50 +0200

    Update equals method

---
+++ RPGCharacters/Helpers/PrimaryAttributes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RPGCharacters.Helpers
+{
+    public class PrimaryAttributes
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+        public int Vitality { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is PrimaryAttributes attributes &&
+                   Strength == attributes.Strength &&
+                   Dexterity == attributes.Dexterity &&
+                   Intelligence == attributes.Intelligence &&
+                   Vitality == attributes.Vitality;
+        }
+
+        public static PrimaryAttributes operator +(PrimaryAttributes a, PrimaryAttributes b) => new()
+        {
+            Strength = a.Strength + b.Strength,
+            Dexterity = a.Dexterity + b.Dexterity,
+            Intelligence = a.Intelligence + b.Intelligence,
+            Vitality = a.Vitality + b.Vitality
+        };
+    }
+}
---
+++ RPGCharacters/Helpers/SecondaryAttributes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RPGCharacters.Helpers
+{
+    public class SecondaryAttributes
+    {
+        public int Health { get; set; }
+        public int ArmorRating { get; set; }
+        public int ElementalResistence { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is SecondaryAttributes attributes &&
+                   Health == attributes.Health &&
+                   ArmorRating == attributes.ArmorRating &&
+                   ElementalResistence == attributes.ElementalResistence;
+        }
+    }
+}
