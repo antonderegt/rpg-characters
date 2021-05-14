4fdf2ac8e46879acad96159674146f54ef8c08e3
Anton de Regt <antonderegt@pm.me>
2021-05-14 13:26:23 +0200

    Add default exception message

---
+++ RPGCharacters/Custom Exceptions/InvalidArmorException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacters.Custom_Exceptions
+{
+    public class InvalidArmorException : Exception
+    {
+        public InvalidArmorException()
+        {
+        }
+
+        public InvalidArmorException(string message) : base(message)
+        {
+        }
+
+        public override string Message => "Invalid armor exception";
+    }
+}
---
+++ RPGCharacters/Custom Exceptions/InvalidWeaponException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacters.Custom_Exceptions
+{
+    public class InvalidWeaponException : Exception
+    {
+        public InvalidWeaponException()
+        {
+        }
+
+        public InvalidWeaponException(string message) : base(message)
+        {
+        }
+
+        public override string Message => "Invalid weapon exception";
+    }
+}
