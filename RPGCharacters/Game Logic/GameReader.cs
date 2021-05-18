cbc84b6a3545631e80adb56c97129520d651ed57
Anton de Regt <antonderegt@pm.me>
2021-05-18 15:12:13 +0200

    Add summary tag

--- RPGCharacters/Game Logic/GameReader.cs
+++ RPGCharacters/Game Logic/GameReader.cs
@@ -4,6 +4,10 @@ namespace RPGCharacters.Game_Logic
 {
     public class GameReader
     {
+        /// <summary>
+        /// Reads player inpyt.
+        /// </summary>
+        /// <returns>Player input string</returns>
         public static string ReadLine()
         {
             return Console.ReadLine();
